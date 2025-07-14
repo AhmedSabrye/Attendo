42b07d4143f8a36d30e4655ea91059f7b02bc3cf
Ahmed Sabry <161619341+AhmedSabrye@users.noreply.github.com>
2025-07-14 14:51:54 +0300

    Refactor: activated authStateChange listener by using it in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
-
+import { useEffect } from "react";
+import { authStateChange } from "./stores/auth.slice";
+import { store } from "./stores/store";
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = authStateChange(store.dispatch);
+    return () => unsubscribe();
+  }, []);
+
   return <RouterProvider router={router} />;
 }
