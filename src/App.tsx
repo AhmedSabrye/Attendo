191a5fb5e285c9c236a0335b338dda36faeb8bab
Ahmed Sabry <161619341+AhmedSabrye@users.noreply.github.com>
2025-07-18 16:35:23 +0300

    Enhance App component: Integrate ToastContainer for notifications.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+
+import { authStateChange, useGetMeQuery } from "./stores/auth.slice";
 import { useEffect } from "react";
-import { authStateChange } from "./stores/auth.slice";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { store } from "./stores/store";
 
 export default function App() {
+  const { refetch } = useGetMeQuery();
+  useEffect(() => {
+    // Get initial session
+    refetch();
+  }, [refetch]);
+
   useEffect(() => {
     const unsubscribe = authStateChange(store.dispatch);
     return () => unsubscribe();
   }, []);
-
-  return <RouterProvider router={router} />;
+  return (
+    <div className="">
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+        stacked
+      />
+    </div>
+  );
 }
