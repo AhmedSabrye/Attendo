028f79fcdacc6d786a4dd01da8f8cdd4ef675225
Ahmed Sabry <161619341+AhmedSabrye@users.noreply.github.com>
2025-07-18 17:51:57 +0300

    Enhance OverviewTable component: Add a serial number column for students and update student mapping to include index for better display. Refactor student link to a div temporary.

--- src/components/overview tab/OverviewTable.tsx
+++ src/components/overview tab/OverviewTable.tsx
@@ -43,6 +43,9 @@ export default function OverviewTable({
     <table className="min-w-full">
       <thead>
         <tr className="border-b border-gray-200  bg-white z-30 sticky top-0">
+          <th className="text-center py-3 px-2 font-medium text-gray-900 bg-white min-w-[40px] text-xs">
+            #
+          </th>
           <th className="text-left py-3 px-4 font-medium text-gray-900  sticky left-0 top-0 z-30 bg-white min-w-[100px] max-w-[120px]">
             Student
           </th>
@@ -67,7 +70,7 @@ export default function OverviewTable({
         </tr>
       </thead>
       <tbody className="divide-y divide-gray-100">
-        {students!.map((student: Student) => {
+        {students!.map((student: Student, index: number) => {
           // Compute rate
           const studentRecords = attendanceMap.get(student.student_id) ?? {};
           const attendedCount = Object.values(studentRecords).filter(
@@ -80,12 +83,13 @@ export default function OverviewTable({
 
           return (
             <tr key={student.student_id} className="hover:bg-gray-50">
+              {/* Number Column */}
+              <td className="text-center py-4 px-2 bg-white z-10 text-xs text-gray-500 font-medium">
+                {index + 1}
+              </td>
               {/* Student Info */}
               <td className=" min-w-[100px] max-w-[150px] py-4 px-4 sticky left-0 bg-white z-10  ">
-                <Link
-                  to={`/groups/${groupId}/students/${student.student_id}`}
-                  className="flex items-center gap-3"
-                >
+                <div className="flex items-center gap-3">
                   <div className="w-8 h-8 bg-emerald-100 rounded-full  items-center justify-center flex-shrink-0 hidden sm:flex">
                     <span className="font-semibold  text-emerald-600 text-sm">
                       {student.name.charAt(0).toUpperCase()}
@@ -99,7 +103,7 @@ export default function OverviewTable({
                       ID: {student.student_id}
                     </p>
                   </div>
-                </Link>
+                </div>
               </td>
 
               {/* Attendance Rate */}
@@ -132,7 +136,7 @@ export default function OverviewTable({
               )}
 
               {/* Attendance Status for the student for each session */}
-              {reports?.map((report, index) => {
+              {sortedReports?.map((report) => {
                 const record = attendanceMap.get(student.student_id)?.[
                   report.date!
                 ];
