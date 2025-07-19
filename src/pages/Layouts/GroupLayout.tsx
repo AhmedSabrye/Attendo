5bca41247d7d43a9dea7a0581db9d7a48e3de2e4
Ahmed Sabry <161619341+AhmedSabrye@users.noreply.github.com>
2025-07-19 12:27:22 +0300

    Refactor: Enhance group creation functionality in GroupsLayout with success notifications and navigation upon successful group creation, solving the problem of not shown createGroupModal when no groups are opened.

--- src/pages/Layouts/GroupLayout.tsx
+++ src/pages/Layouts/GroupLayout.tsx
@@ -3,21 +3,15 @@ import GroupHeader from "@/components/GroupHeader";
 import AttendanceSheetModal from "@/components/modals/AttendanceSheetModal/AttendanceSheetModal";
 import { useState } from "react";
 import TemplateManager from "@/components/modals/TemplateManager/TemplateManager";
-import { useCreateGroupMutation, useGetCurrentUserQuery, useGetStudentsQuery } from "@/stores/api";
+import { useGetStudentsQuery } from "@/stores/api";
 import { toast } from "react-toastify";
-import { useAppDispatch, useAppSelector } from "@/stores/hooks";
-import CreateGroupModal from "@/components/modals/CreateGroupModal";
-import { toggleCreateGroupModal } from "@/stores/modals";
+import { useAppSelector } from "@/stores/hooks";
 
 export default function GroupLayout() {
   const { groupId } = useParams();
-  const { sidebarOpen, createGroupModalOpen } = useAppSelector((state) => state.modals);
-  const dispatch = useAppDispatch();
+  const { sidebarOpen } = useAppSelector((state) => state.modals);
   const [modal, setModal] = useState<0 | 1 | 2>(0);
   const { data: students } = useGetStudentsQuery(Number(groupId));
-  const { user: authUser } = useAppSelector((state) => state.auth);
-  const [createGroupMutation] = useCreateGroupMutation();
-  const { data: currentUser } = useGetCurrentUserQuery(authUser);
 
   function onOpenTemplateManagerModal() {
     setModal(1);
@@ -38,19 +32,6 @@ export default function GroupLayout() {
     setModal(0);
   }
 
-
-  const handleCreateGroup = async (groupData: { name: string }) => {
-    if (!currentUser) return;
-    try {
-      await createGroupMutation({
-        userId: Number(currentUser.user_id),
-        groupName: groupData.name,
-      }).unwrap();
-    } catch (error) {
-      console.error("Error creating group:", error);
-    }
-  };
-
   return (
     <div
       className={`md:p-4 p-2 ${sidebarOpen ? "overflow-hidden h-screen" : ""}`}
@@ -68,18 +49,9 @@ export default function GroupLayout() {
       )}
       {modal === 1 && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
-          <TemplateManager
-            onClose={onCloseModal}
-          />
+          <TemplateManager onClose={onCloseModal} />
         </div>
       )}
-      {createGroupModalOpen && (
-          <CreateGroupModal
-            isOpen={createGroupModalOpen}
-            onClose={() => dispatch(toggleCreateGroupModal())}
-            onCreateGroup={handleCreateGroup}
-          />
-        )}
     </div>
   );
 }
--- src/pages/Layouts/GroupsLayout.tsx
+++ src/pages/Layouts/GroupsLayout.tsx
@@ -1,7 +1,32 @@
 import { Outlet } from "react-router";
 import GroupsSidebar from "../../components/Sidebar";
+import CreateGroupModal from "@/components/modals/CreateGroupModal";
+import { toggleCreateGroupModal } from "@/stores/modals";
+import { useAppDispatch, useAppSelector } from "@/stores/hooks";
+import { useCreateGroupMutation, useGetCurrentUserQuery } from "@/stores/api";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
 
 export default function GroupsLayout() {
+  const navigate = useNavigate();
+  const { createGroupModalOpen } = useAppSelector((state) => state.modals);
+  const dispatch = useAppDispatch();
+  const { user: authUser } = useAppSelector((state) => state.auth);
+  const [createGroupMutation] = useCreateGroupMutation();
+  const { data: currentUser } = useGetCurrentUserQuery(authUser);
+  const handleCreateGroup = async (groupData: { name: string }) => {
+    if (!currentUser) return;
+    try {
+      const res = await createGroupMutation({
+        userId: Number(currentUser.user_id),
+        groupName: groupData.name,
+      }).unwrap();
+      toast.success("Group created successfully");
+      navigate(`/groups/${res.group_id}/overview`);
+    } catch (error) {
+      console.error("Error creating group:", error);
+    }
+  };
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
@@ -10,6 +35,13 @@ export default function GroupsLayout() {
           <Outlet />
         </div>
       </div>
+      {createGroupModalOpen && (
+          <CreateGroupModal
+            isOpen={createGroupModalOpen}
+            onClose={() => dispatch(toggleCreateGroupModal())}
+            onCreateGroup={handleCreateGroup}
+          />
+        )}
     </div>
   );
 }
